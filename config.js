87a6f90943be80bd07ac34dcc47cfe88c723d4b8
Darren Le <darrenle98@gmail.com>
2019-12-05 00:02:40 -0500

    fixing config

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 const config = {
-  host: "us-cdbr-iron-east-05.cleardb.net",
-<<<<<<< HEAD
-  port: 3306,
-=======
+  host: "localhost",
   port: 8889,
->>>>>>> 49305c8ea777f4c096698599d89ed33aa851639a
-  uname: "b619a1c743fb12",
-  pword: "6bd2c705",
-  database: "heroku_90d6c81e79485c3"
+  uname: "root",
+  pword: "root",
+  database: "db_portfolio"
 };
 
 module.exports = config;
