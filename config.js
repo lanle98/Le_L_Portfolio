65eeca19a1e243579a3d5e87760e3f2c9e16d8ad
Darren Le <darrenle98@gmail.com>
2019-12-05 13:19:48 -0500

    finxing database

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-  host: "localhost",
+  host: "us-cdbr-iron-east-05.cleardb.net",
   port: 8889,
-  uname: "root",
-  pword: "root",
-  database: "db_portfolio"
+  uname: "b619a1c743fb12",
+  pword: "6bd2c705",
+  database: "heroku_90d6c81e79485c3"
 };
 
 module.exports = config;
