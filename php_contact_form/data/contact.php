3b072794c526fa8156e50514206a8e9133d8ef96
Darren Le <darrenle98@gmail.com>
2019-12-07 17:18:25 -0500

    added php_contact_form

---
+++ php_contact_form/data/contact.php
@@ -0,0 +1,51 @@
+<?php
+
+//var_dump($_POST['name']);
+//var_dump($_GET);
+
+if (empty($_POST)) {
+    echo 'no....';
+    exit;
+}
+
+//validate all data
+$name = '';
+$email = '';
+$subject = '';
+$message = '';
+$recipent = 'darrenle98@gmail.com';
+
+if (isset($_POST['name'])) {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+}
+
+if (isset($_POST['email'])) {
+    $email = str_replace(array('\r', '\n', '%0a', '%0d'), '', $_POST['email']);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+if (isset($_POST['subject'])) {
+    $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+
+}
+
+if (isset($_POST['message'])) {
+    $message = $_POST['message'];
+}
+
+//Send out email
+$header = array(
+    'From' => 'noreply@test.ca',
+    'Reply-To' => $name . '<' . $email . '>',
+);
+
+if (mail($recipent, $subject, $message, $header)) {
+    echo '<p>Thank you for contacting us, ' . $name . '</p>';
+
+} else {
+    echo '<p>We are sorry but the mail did not go through</p>';
+}
+
+//how to redirect user in php ???
+//how do we show php popup ??
+//use capcha for portfolio
