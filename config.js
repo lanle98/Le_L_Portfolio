7f84e3667e30f321489ddeb442968f174781af5c
Darren Le <darrenle98@gmail.com>
2019-12-13 00:26:26 -0500

    changes port

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const config = {
-  host: "localhost",
-  port: 8889,
-  uname: "root",
-  pword: "root",
-  database: "db_portfolio"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  uname: "b284601459d0a6",
+  pword: "beeca44c",
+  database: "heroku_c5d775299984acb"
 };
 
 module.exports = config;
